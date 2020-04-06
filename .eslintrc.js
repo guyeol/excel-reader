382ee1b63a4acc7576236e660841c62298f7c953
guyeol, jeong <jigu8534@gmail.com>
2020-04-06 23:48:51 +0900

    fix: eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
   rules: {
     // A temporary hack related to IDE not resolving correct package.json
     'import/no-extraneous-dependencies': 'off',
-    "no-restricted-syntax": ["error", "LabeledStatement", "WithStatement"]
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+    'import/no-named-as-default': 0,
+    'no-loop-func': 1
   },
   settings: {
     'import/resolver': {
